6442f3e611500505620ecced30723705381e5ef0
Sahan Ratnayake <sahanratnayake18@gmail.com>
2019-12-11 12:04:10 +0400

    app.js changed

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          WORKING React
+          Learn React
         </a>
       </header>
     </div>
